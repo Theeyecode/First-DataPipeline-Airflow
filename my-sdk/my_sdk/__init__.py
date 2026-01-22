69b9dcc932a1ebdc56f08930ffc19fbfcfd78afe
Emmanuel Olajubu <emmanuelolajubu90@gmai.com>
2026-01-22 09:54:01 -0500

    create sql query

---
+++ my-sdk/my_sdk/__init__.py
@@ -0,0 +1,19 @@
+
+
+from typing import Any
+
+__version__ = "0.0.1"
+__all__ = ["sql"]
+
+def get_provider_info() -> dict[str, Any]:
+    return {
+        "name": "My SDK",
+        "version": [__version__],
+        "description": "My SDK is a package that provides a set of tools for buildin Airflow DAGs.",
+        "task-decorators":[
+            {
+                "name": "sql",
+                "class-name": "my_sdk.decorators.sql.sql_task"
+            }
+        ]
+    }
